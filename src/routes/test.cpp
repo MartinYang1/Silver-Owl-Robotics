9beeaa39278d728b321baed8e8e9eafab9bfadfd
Martin Yang <martinyang29@gmail.com>
2024-06-01 17:31:17 -0400

    Tuned PID more

--- src/routes/test.cpp
+++ src/routes/test.cpp
@@ -8,18 +8,19 @@
 
 void test() {
     // declare initial conditions
-    PIDDataSet TestPara={4,0.1,0.2};
-
-    // moves forward
-    MoveEncoderPID(TestPara, -100 , 24 ,0.2,0,true);
-    // turns to 90 degrees, 0.4s for calculations, brake when heading reached
-    TurnMaxTimePID(TestPara, 90, 0.4, true);
-    // Move backwards at 80 power, 10 inches, 0.2s to max speed, 
-    MoveEncoderPID(TestPara, 80 , 10 ,0.2,90,true);
-    // Turn to 60 degrees, 0.4s for calculations, braking
-    TurnMaxTimePID(TestPara, 60, 0.4, true);
-    // Move foward at 50 power, for 10 inches, 0.2 to accelerate to max speed, absolute heading, braking
-    MoveEncoderPID(TestPara, -50 , 10 ,0.2,60,true);
+    //PIDDataSet TestPara={4,0.1,0.2};
+    PIDDataSet TestPara={1.5,0.1,0.15};
+    MoveEncoderPID(TestPara, -50 , 120 ,0.4,0,true);
+    // // moves forward
+    // MoveEncoderPID(TestPara, -100 , 24 ,0.2,0,true);
+    // // turns to 90 degrees, 0.4s for calculations, brake when heading reached
+    // TurnMaxTimePID(TestPara, 90, 0.4, true);
+    // // Move backwards at 80 power, 10 inches, 0.2s to max speed, 
+    // MoveEncoderPID(TestPara, 80 , 10 ,0.2,90,true);
+    // // Turn to 60 degrees, 0.4s for calculations, braking
+    // TurnMaxTimePID(TestPara, 60, 0.4, true);
+    // // Move foward at 50 power, for 10 inches, 0.2 to accelerate to max speed, absolute heading, braking
+    // MoveEncoderPID(TestPara, -50 , 10 ,0.2,60,true);
 
     //MoveEncoderPID(TestPara, 100 , 560 ,0.2,0,true);
 }
